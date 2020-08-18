5112532c7798491b9906e848ca8d613cff168d21
Aiden Siegle <aidensiegle@gmail.com>
2020-08-18 16:42:05 -0700

    Initial Commit

---
+++ Fish Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fish Timer
+//
+//  Created by Aiden Siegle on 8/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
