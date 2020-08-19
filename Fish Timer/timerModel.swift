7767e55385722bdb69dfb6517821c6c0a1ad4cd2
Aiden Siegle <aidensiegle@gmail.com>
2020-08-19 16:08:49 -0700

    Model started

---
+++ Fish Timer/timerModel.swift
@@ -0,0 +1,31 @@
+//
+//  timerModel.swift
+//  Fish Timer
+//
+//  Created by Aiden Siegle on 8/19/20.
+//
+
+import Foundation
+import UIKit
+
+struct foodType {
+    var index = 1
+    var name: String
+    var times = [Int]()
+    var images = [UIImage]()
+}
+
+
+class timerModel {
+    //time in seconds
+    // [undercooked, cooked, burnt, fire]
+    // Burning for 5 mins (300 seconds) causes fire
+    var foodTimes = ["fish"  :[30,40,80, 380],
+                     "trophy":[80,90,180, 480],
+                     "meat"  :[50,60,120, 420],
+                     "meg"   :[100,120,240, 540],
+                     "kraken":[100,120,240, 540],
+                     "fruit" :[0,0,15, 315],
+                     "bait"  :[0,0,10, 310]]
+
+}
