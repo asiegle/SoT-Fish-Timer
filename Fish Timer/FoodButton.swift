eeb388660bdc7450253e9232347a54e8ef89c9c7
Aiden Siegle <aidensiegle@gmail.com>
2020-08-20 16:14:32 -0700

    Button class, editable labels

---
+++ Fish Timer/FoodButton.swift
@@ -0,0 +1,29 @@
+//
+//  FoodButton.swift
+//  Fish Timer
+//
+//  Created by Aiden Siegle on 8/20/20.
+//
+
+import UIKit
+
+class FoodButton: UIStackView {
+    
+//    var label: UILabel!
+//    var image: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+     
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Fish Timer/ViewController.swift
+++ Fish Timer/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var FoodButtons: [FoodButton]!
+    @IBOutlet weak var timer: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FoodButtons[0].label.text = "Fish"
+        FoodButtons[1].label.text = "Trophy Fish"
+
         // Do any additional setup after loading the view.
+        
     }
 
 
