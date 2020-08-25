f498f77e3f95beee3f3ed827221581929d27adef
Aiden Siegle <aidensiegle@gmail.com>
2020-08-25 16:44:41 -0700

    Functioning timer with selectable times

--- Fish Timer/ViewController.swift
+++ Fish Timer/ViewController.swift
@@ -6,16 +6,29 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     
     @IBOutlet var FoodButtons: [FoodButton]!
-    @IBOutlet weak var timer: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var currentTime = 0
+    var timer = Timer()
+    
+    let foodTimes = ["Fish"  :[30,40,80, 380],
+                     "Trophy Fish":[80,90,180, 480],
+                     "meat"  :[50,60,120, 420],
+                     "meg"   :[100,120,240, 540],
+                     "kraken":[100,120,240, 540],
+                     "fruit" :[0,0,15, 315],
+                     "bait"  :[0,0,10, 310]]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         FoodButtons[0].label.text = "Fish"
         FoodButtons[0].image.image = UIImage(named: "fish.png")
@@ -23,15 +36,42 @@ class ViewController: UIViewController {
         FoodButtons[1].image.image = UIImage(named: "fishTrophy.png")
         
 
-        // Do any additional setup after loading the view.
         
     }
-        
     
+    @objc func tickTimer() {
+        currentTime -= 1
+        timerLabel.text = String(currentTime)
+
+        print(currentTime)
+        
+                
+        if (currentTime == 0) {
+            timerLabel.text = "Done!"
+            timer.invalidate()
+        }
+        
+
+    }
+
+    //each stack has it's own Tap recognizer, but they share an action
     @IBAction func onTap(_ sender: UITapGestureRecognizer) {
-//        print("tap")
+        timer.invalidate()
+        
         let button = sender.view as? FoodButton
-        print(button?.label.text ?? "Error")
+        let name = button?.label.text ?? "Error"
+        print(name)
+        
+        currentTime = foodTimes[name]?[1] ?? 0
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(tickTimer), userInfo: button, repeats: true)
+        timerLabel.text = String(currentTime)
+
+        RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
+        
+        
+       
+
+
     }
     
 
--- Fish Timer/timerModel.swift
+++ Fish Timer/timerModel.swift
@@ -20,9 +20,9 @@ class timerModel {
     //time in seconds
     // [undercooked, cooked, burnt, fire]
     // Burning for 5 mins (300 seconds) causes fire
-    var foodTimes = ["fish"  :[30,40,80, 380],
-                     "trophy":[80,90,180, 480],
-                     "meat"  :[50,60,120, 420],
+    var foodTimes = ["Fish"  :[30,40,80, 380],
+                     "Trophy Fish":[80,90,180, 480],
+                     "Meat"  :[50,60,120, 420],
                      "meg"   :[100,120,240, 540],
                      "kraken":[100,120,240, 540],
                      "fruit" :[0,0,15, 315],
